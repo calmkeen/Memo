af973377a67a400a9a27e6b99f14f0c841dc7081
calmkeen <iyunseong159@gmail.com>
2022-03-29 14:10:07 +0900

    Initial Commit

---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by calmkeen on 2022/03/29.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
